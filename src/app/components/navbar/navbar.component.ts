2e2e1ce03a6beaede86f7b19cf3081742d948914
gabriel-arroyo <ing.gabriel.arroyo@gmail.com>
2022-04-18 05:36:53 -0500

    process reactive

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -8,16 +8,15 @@ import { Component, OnInit, Input } from '@angular/core';
 export class NavbarComponent implements OnInit {
   @Input() logo: string = "https://firebasestorage.googleapis.com/v0/b/claudia-oropeza-photography.appspot.com/o/logo.png?alt=media&token=c7a7d4b5-f61e-41b2-9d1f-a5794220bf57"
   @Input() navitems: any[] = [
-    {title:'Home',link:'#cover'},
-    {title:'Why Us', link:'/whyus'},
-    {title:'Our Dumpsters', link:'#dumpsters'},
+    { title: 'Home', link: '#cover' },
+    { title: 'Why Us', link: '/whyus' },
+    { title: 'Our Dumpsters', link: '#dumpsters' },
   ]
   selected: number = 0;
 
   constructor() { }
 
   select(n: number) {
-    console.log("test", n)
     this.selected = n
   }
 
--- src/app/components/processbar/processbar.component.ts
+++ src/app/components/processbar/processbar.component.ts
@@ -7,21 +7,26 @@ import { Process } from 'src/app/models/process';
   styleUrls: ['./processbar.component.scss']
 })
 export class ProcessbarComponent implements OnInit {
-  @Input() processes:Process[]=[
+  @Input() processes: Process[] = [
     {
-      imageSrc:'../../../assets/images/reserve.png',
-      title:'Reserve',
-      description:'Consectetur non aliqua et exercitation consectetur aute sunt aliqua et eu qui consequat.'
+      imageSrc: '../../../assets/images/reserve-icon.png',
+      title: 'Reserve',
+      description: 'Consectetur non aliqua et exercitation consectetur aute sunt aliqua et eu qui consequat.'
     },
     {
-      imageSrc:'../../../assets/images/buy.png',
-      title:'Buy',
-      description:'Labore velit consequat minim cupidatat excepteur aliqua occaecat officia officia do.'
+      imageSrc: '../../../assets/images/buy-icon.png',
+      title: 'Buy',
+      description: 'Labore velit consequat minim cupidatat excepteur aliqua occaecat officia officia do.'
     },
     {
-      imageSrc:'../../../assets/images/deliver.png',
-      title:'Deliver',
-      description:'Do eu laboris commodo sunt ipsum duis veniam et ea ex anim.'
+      imageSrc: '../../../assets/images/deliver-icon.png',
+      title: 'Deliver',
+      description: 'Do eu laboris commodo sunt ipsum duis veniam et ea ex anim.'
+    },
+    {
+      imageSrc: '../../../assets/images/pickup-icon.png',
+      title: 'Pickup',
+      description: 'Do eu laboris commodo sunt ipsum duis veniam et ea ex anim.'
     }
   ]
 
