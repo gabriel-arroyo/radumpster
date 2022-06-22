1aadfd3efe47e737e3ac97dcbd6e3cb7a1cef215
Gabriel Arroyo <gabriel_eleazar.arroyo@daimler.com>
2022-06-22 15:31:07 -0500

    blue background body

--- src/app/components/section-title/section-title.component.ts
+++ src/app/components/section-title/section-title.component.ts
@@ -7,6 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class SectionTitleComponent implements OnInit {
   @Input() title: string = ""
+  @Input() color: string = "light"
   constructor() { }
 
   ngOnInit(): void {
